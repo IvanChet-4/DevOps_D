d77294ea12a7d8373a4aff7727d7ab0587555d21
Ivan Chetverkin <104260618+IvanChet-4@users.noreply.github.com>
2025-07-26 03:58:52 +0300

    Create variables.tf

---
+++ Terraform/Project/backend_setup/variables.tf
@@ -0,0 +1,24 @@
+variable "bucket_name" {
+  type = string
+  default = "private-bucket-state"
+}
+
+variable "zone" {
+  type = string
+  default = "ru-central1-a"
+}
+
+variable "folder_id" {
+  type = string
+  default = ""
+}
+
+variable "cloud_id" {
+  type = string
+  default = ""
+}
+
+variable "yandex_token" {
+  type = string
+  default = ""
+}
