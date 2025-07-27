fb388307282d14ceb26b0c8229588d47e74408bf
Ivan Chetverkin <104260618+IvanChet-4@users.noreply.github.com>
2025-07-27 15:45:18 +0300

    Update main.tf

--- Terraform/Project/setup_bucket/main.tf
+++ Terraform/Project/setup_bucket/main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-  required_version = ">= 0.13"
-}
-
-provider "yandex" {
-  token     = var.yandex_token
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = var.zone
-}
-
 resource "yandex_storage_bucket" "private_bucket" {
   bucket = var.bucket_name
   folder_id = var.folder_id
