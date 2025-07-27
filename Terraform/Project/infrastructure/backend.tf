d22f61eb422c9b4fb91c6084f329eb30118ec31a
Ivan Chetverkin <104260618+IvanChet-4@users.noreply.github.com>
2025-07-27 17:45:01 +0300

    Update backend.tf

--- Terraform/Project/infrastructure/backend.tf
+++ Terraform/Project/infrastructure/backend.tf
@@ -1,15 +1,11 @@
 terraform {
   required_version = ">= 0.13"
   backend "s3" {
-    endpoints = {
-      s3 = "https://storage.yandexcloud.net"
-    }
+    endpoints = "https://storage.yandexcloud.net"
     bucket ="private-bucket-state"
     key        = "1.tfstate"
-    region     = "us-east-1"
+    region     = "ru-central1"
     skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_requesting_account_id = true
-    skip_s3_checksum            = true
+    skip_credentials_validation = false
   }
 }
